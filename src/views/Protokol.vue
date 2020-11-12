8f6817889aab1dc2293dab47acb0babb273fddc4
Ananta Ihza Ramadhan <anantaihza21@gmail.com>
2020-11-12 11:44:52 +0700

    Update protocol page

--- src/views/Protokol.vue
+++ src/views/Protokol.vue
@@ -9,44 +9,44 @@
     </section>
     <section class="tips py-5">
       <b-container>
-        <h2 class="text-center pb-2">Tips - tips Pencegahan Penyebaran COVID-19</h2>
+        <h2 class="text-center pb-2">Tips Pencegahan Penyebaran COVID-19</h2>
         <p
           class="text-center"
         >Beberapa hal yang dapat dilakukan untuk mencegah penyebaran Virus Corona :</p>
         <b-row class="pt-3 text-center">
           <b-col cols="12" md="6" lg="4">
             <b-card class="card">
-              <img src="@/assets/protokol/sentuhan.svg" alt="">
+              <img src="@/assets/protokol/sentuhan.svg" alt />
               <p>Jangan sentuh mata, hidung, atau mulut Anda.</p>
             </b-card>
           </b-col>
           <b-col cols="12" md="6" lg="4">
             <b-card class="card">
-              <img src="@/assets/protokol/login.svg" alt="">
-              <p>Jangan keluar rumah jika tidak ada kebutuhan yang mendesak</p>
+              <img src="@/assets/protokol/login.svg" alt />
+              <p>Jangan keluar rumah jika tidak ada kebutuhan yang mendesak.</p>
             </b-card>
           </b-col>
           <b-col cols="12" md="6" lg="4">
             <b-card class="card">
-              <img src="@/assets/protokol/medic.svg" alt="">
-              <p>Jika anda merasa terkena gejala berat, segera cari bantuan medis. </p>
+              <img src="@/assets/protokol/medic.svg" alt />
+              <p>Jika anda merasa terkena gejala berat, segera cari bantuan medis.</p>
             </b-card>
           </b-col>
           <b-col cols="12" md="6" lg="4">
             <b-card class="card">
-              <img src="@/assets/protokol/distancing.svg" alt="">
-              <p>Selalu kenakan masker pada saat diluar rumah</p>
+              <img src="@/assets/protokol/distancing.svg" alt />
+              <p>Selalu kenakan masker pada saat diluar rumah.</p>
             </b-card>
           </b-col>
           <b-col cols="12" md="6" lg="4">
             <b-card class="card">
-              <img src="@/assets/protokol/wash.svg" alt="">
-              <p>Cuci tangan  secara rutin. Gunakan sabun atau cairan pembersih tangan berbahan alkohol pada saat mencuci tangan.</p>
+              <img src="@/assets/protokol/wash.svg" alt />
+              <p>Cuci tangan secara rutin dan gunakan sabun atau hand sanitizer.</p>
             </b-card>
           </b-col>
           <b-col cols="12" md="6" lg="4">
             <b-card class="card">
-              <img src="@/assets/protokol/empty.svg" alt="">
+              <img src="@/assets/protokol/empty.svg" alt />
               <p>Melakukan social distancing dan jaga jarak aman dengan orang lain.</p>
             </b-card>
           </b-col>
@@ -58,7 +58,7 @@
         <h2 class="text-center pb-5">Perawatan Diri</h2>
         <b-row class="text-justify">
           <b-col sm="12" md="4">
-            <p>Jika Anda merasa sakit  segera beristirahat, minum banyak air, dan makan makanan bergizi. Gunakan ruangan yang terpisah dari anggota keluarga Anda, dan jika memungkinkan gunakan kamar mandi khusus. Bersihkan dan lakukan desinfeksi pada permukaan benda yang sering disentuh. Usahakan untuk selalu menjaga rutinitas dan jadwal seperti biasanya.</p>
+            <p>Jika Anda merasa sakit segera beristirahat, minum banyak air, dan makan makanan bergizi. Gunakan ruangan yang terpisah dari anggota keluarga Anda, dan jika memungkinkan gunakan kamar mandi khusus. Bersihkan dan lakukan desinfeksi pada permukaan benda yang sering disentuh. Usahakan untuk selalu menjaga rutinitas dan jadwal seperti biasanya.</p>
           </b-col>
           <b-col sm="12" md="4">
             <p>Semua orang harus menjaga pola hidup sehat di rumah. Jaga pola diet yang sehat, tidur cukup, tetap aktif, dan lakukan kontak sosial dengan orang-orang yang Anda sayangi melalui telepon atau internet. Selama masa sulit seperti sekarang, anak-anak membutuhkan kasih sayang dan perhatian lebih dari orang dewasa.</p>
@@ -72,6 +72,80 @@
     <section class="produktif py-5">
       <b-container>
         <h2 class="text-center pb-5">Hal Produktif yang Dapat Dilakukan</h2>
+        <b-row class="pt-2">
+          <b-col lg="6">
+            <b-card class="hal-card">
+              <b-row>
+                <b-col cols="5" sm="4" class="my-auto">
+                  <img src="@/assets/protokol/hal-book.svg" alt width="100%" />
+                </b-col>
+                <b-col cols="7" sm="8" class="my-auto">
+                  <p>Baca buku kesukaanmu yang tentunya tidak hanya menambahkan ilmu pengetahuan namun juga melatih kecepatan membacamu.</p>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+          <b-col lg="6">
+            <b-card class="hal-card">
+              <b-row>
+                <b-col cols="5" sm="4" class="my-auto">
+                  <img src="@/assets/protokol/hal-invest.svg" alt width="100%" />
+                </b-col>
+                <b-col cols="7" sm="8" class="my-auto">
+                  <p>Mulailah investasi jangka panjang berupa pengetahuan maupun dalam bentuk materi untuk ningkatkan kualitas di masa depan.</p>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+          <b-col lg="6">
+            <b-card class="hal-card">
+              <b-row>
+                <b-col cols="5" sm="4" class="my-auto">
+                  <img src="@/assets/protokol/hal-cook.svg" alt width="100%" />
+                </b-col>
+                <b-col cols="7" sm="8" class="my-auto">
+                  <p>Memasak juga dapat memanfaatkan waktumu yang kosong loh tidak hanya menyenangkan namun juga kamu bisa belajar memasak makanan yang baru.</p>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+          <b-col lg="6">
+            <b-card class="hal-card">
+              <b-row>
+                <b-col cols="5" sm="4" class="my-auto">
+                  <img src="@/assets/protokol/hal-perawatan.svg" alt width="100%" />
+                </b-col>
+                <b-col cols="7" sm="8" class="my-auto">
+                  <p>Perawatan juga membutuhkan waktu banyak loh jadi kamu bisa memanfaatkan waktu luangmu dengan merawat muka dan tubuh agar kamu lebih ganteng dan cantik.</p>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+          <b-col lg="6">
+            <b-card class="hal-card">
+              <b-row>
+                <b-col cols="5" sm="4" class="my-auto">
+                  <img src="@/assets/protokol/hal-write.svg" alt width="100%" />
+                </b-col>
+                <b-col cols="7" sm="8" class="my-auto">
+                  <p>Selama ini mungkin kamu tidak mempunyai waktu luang untuk menulis dengan dirumah aja sekarang kamu bisa menulis diary bahkan menulis life planning yang bermanfaat bagi masa depanmu.</p>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+          <b-col lg="6">
+            <b-card class="hal-card">
+              <b-row>
+                <b-col cols="5" sm="4" class="my-auto">
+                  <img src="@/assets/protokol/hal-yoga.svg" alt width="100%" />
+                </b-col>
+                <b-col cols="7" class="my-auto">
+                  <p>Tidak semua olahraga diluar rumah loh kamu juga bisa melakukan yoga dirumah yang akan memberikan kamu ketenangan jiwa dan kebugaran jasmani.</p>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+        </b-row>
       </b-container>
     </section>
   </div>
@@ -101,7 +175,7 @@ export default {
 .intro p {
   font-weight: 500;
 }
-.tips h2, 
+.tips h2,
 .self-care h2,
 .produktif h2 {
   font-weight: 700;
@@ -111,11 +185,18 @@ export default {
   background: rgb(235, 246, 250);
 }
 .card img {
-    width: 80%;
-    display: block;
-    margin: 20px auto;
-    height: 8rem;
-    object-fit: contain;
+  width: 80%;
+  display: block;
+  margin: 20px auto;
+  height: 8rem;
+  object-fit: contain;
+}
+.hal-card img {
+  width: 100%;
+  display: block;
+  margin: auto;
+  height: 8rem;
+  object-fit: contain;
 }
 .self-care {
   color: white;
